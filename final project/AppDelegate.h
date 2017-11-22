e8399ecf246813bc58d26b82c32afb4fafb48593
Rataj <rataj@ratajs-macbook-pro.local>
2017-11-22 22:17:09 +0000

    Initial Commit

---
+++ final project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  final project
+//
+//  Created by Rataj on 11/22/17.
+//  Copyright © 2017 Rataj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ final project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  final project
+//
+//  Created by Rataj on 11/22/17.
+//  Copyright © 2017 Rataj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
